36f8895f757ece8e86df10154cde079b5534a0d9
deokbin <deokbin@naver.com>
2024-06-10 00:22:04 +0900

    이미 작성한 일기가 있을 경우 데이터 조회해서 불러옴

--- diary/src/DiaryEditPage.jsx
+++ diary/src/DiaryEditPage.jsx
@@ -9,24 +9,34 @@ export function DiaryEdit() {
 
     const ReturnCalendar = () => {navigate("/");}
 
-    const [text, setText] = useState('');
-
+    //date 가 있으면 해당 날짜의 일기를 불러옴, 없으면 빈 문자열
+    const savedText = date ? localStorage.getItem(date) : '';
+   
+    // savedText 값이 존재한다면 해당 값을 초기값으로 설정하고, 그렇지 않으면 빈 문자열('')을 초기값으로 설정합니다.
+    const [text, setText] = useState(savedText || '');
+   
+    // handleDiaryChange 함수는 textarea의 입력값이 변경될 때마다 호출되어 해당 입력값을 text 상태로 업데이트합니다.
     const handleDiaryChange = (event) => {
         setText(event.target.value);
     }
-
-// 저장 로직 따로 구현 (아래에서 단순히 useLocalStorage 호출 불가))
+    // 함수로 호출하였을 때 useEffect가 호출되지 않는 문제가 있음 -> 수정 완료
+    // 저장 로직 따로 구현 (아래에서 단순히 함수로 useLocalStorage 호출 불가))
     const handleSave = () => {
         // 직접 localStorage에 값을 저장
         if (date && text) {
             localStorage.setItem(date, text);
+            alert('일기가 저장되었습니다.');
+
         }
     }
 
     const handleRemove = () => {
         // 직접 localStorage에 값에서 삭제
             localStorage.removeItem(date, text);
+            alert('일기가 삭제되었습니다.');
         }
+    
+    //localstorage에 저장된 값이 있는 경우 불러오기
     
 
 
@@ -63,5 +73,3 @@ export function DiaryEdit() {
 
     )
 }
-
-
