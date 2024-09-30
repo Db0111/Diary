c8b629a906cc4bc3f3841472016610fa15781f30
deokbin <deokbin@naver.com>
2024-09-30 16:38:45 +0900

    vite.config.js base 설정

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5144'
-    }
-  }
-})
+      "/api": "http://localhost:5144",
+    },
+  },
+});
